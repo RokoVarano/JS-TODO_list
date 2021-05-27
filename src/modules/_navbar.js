8e8bc351c381ae429ed2e267e3383f67bf77da37
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-27 13:19:52 -0400

    Add a navbar

---
+++ src/modules/_navbar.js
@@ -0,0 +1,16 @@
+
+export default function navbar() {
+  const navbar = document.createElement('nav');
+  navbar.classList.add('main');
+
+  const link = document.createElement('a');
+  link.href = '';
+  navbar.appendChild(link);
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Tasker!';
+  navbar.appendChild(h1);
+
+  return navbar;
+}
+
