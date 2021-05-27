3534ac13b96dd894ac5e1dbcde419d622b8e3131
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-27 23:15:28 +0300

    fixup! Merge branch 'feature/global_styling' into feature/form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './styles.scss';
-import navbar from './modules/_navbar';
+
 import { projectForm, taskForm } from './modules/_form';
+
+import navbar from './modules/_navbar';
 
 const index = () => {
   const { body } = document;
 
+  body.appendChild(navbar());
   body.appendChild(taskForm());
   body.appendChild(projectForm());
-  body.appendChild(navbar());
   return body;
 };
 
