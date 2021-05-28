9871e4a915f436551669f5420fe596bc7a14140a
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-28 20:59:00 +0300

    Add add projects

--- src/modules/_localStorage.js
+++ src/modules/_localStorage.js
@@ -1,13 +1,27 @@
-class Project {
+class ProjectMemory {
   getAllProjects = () => {
     let projects;
-    if (localStorage.getItem('projects') === null) {
+    if (localStorage.getItem('projects') === null || undefined) {
       projects = [];
     } else {
       projects = JSON.parse(localStorage.getItem('projects'));
     }
     return projects;
   };
+
+  addProject = (project) => {
+    let projects;
+    if (localStorage.getItem('projects') === null || undefined) {
+      projects = [];
+    } else {
+      projects = JSON.parse(localStorage.getItem('projects'));
+    }
+    projects.push(project);
+    project.id = projects.indexOf(project);
+    localStorage.setItem('projects', JSON.stringify(projects));
+
+    return projects;
+  };
 }
 
-export default Project;
+export default ProjectMemory;
