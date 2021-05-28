1230013840c6949887181ad40b8667cf1cb8a82a
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-28 12:12:33 -0400

    Add Project Widget with delete button

--- src/modules/_navbar.js
+++ src/modules/_navbar.js
@@ -1,26 +1,36 @@
 import './_Project_class';
 import { projectForm } from './_form';
-import { projectPool } from './_projectPool';
+import { removeFromPool, projectPool } from './_projectPool';
 
 const projectWidget = (project) => {
   const container = document.createElement('div');
+  container.classList.add('list-item');
   const h3 = document.createElement('h3');
   h3.textContent = project.title;
   container.appendChild(h3);
-
+  const xButton = document.createElement('button');
+  xButton.classList.add('x-button');
+  xButton.textContent = 'X';
+  xButton.onclick = (() => removeWidget(project));
+  container.appendChild(xButton);
   return container;
 };
 
-function setHomepage() {
+const setHomepage = () => {
   const main = document.querySelector('main');
   main.innerHTML = '';
 
   main.appendChild(projectForm());
 
+  const listContainer = document.createElement('div');
+  listContainer.classList.add('list-container');
+
   projectPool.map(
-    (project) => main.appendChild(projectWidget(project)),
+    (project) => listContainer.appendChild(projectWidget(project)),
   );
-}
+
+  main.appendChild(listContainer);
+};
 
 export default function navbar() {
   const navbar = document.createElement('nav');
@@ -33,3 +43,8 @@ export default function navbar() {
 
   return navbar;
 }
+
+const removeWidget = (project) => {
+  removeFromPool(project);
+  setHomepage();
+};
--- src/modules/_projectPool.js
+++ src/modules/_projectPool.js
@@ -1,11 +1,13 @@
 import Project from './_Project_class';
 
-export const projectPool = [];
+const projectPool = [new Project('Dog'), new Project('Cat')];
 
-export const addToPool = (title) => {
+export default function addToPool(title) {
   projectPool.push(new Project(title));
-};
+}
 
-export const removeFromPool = (project) => {
+function removeFromPool(project) {
   projectPool.splice(projectPool.indexOf(project), 1);
-};
+}
+
+export { projectPool, addToPool, removeFromPool };
