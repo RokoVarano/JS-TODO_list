caa217cbfcf522ae37215b7e207e0a3253ebc3ba
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-31 17:26:03 -0400

    Task side stylings

--- src/index.js
+++ src/index.js
@@ -14,13 +14,21 @@ const index = () => {
   body.appendChild(main);
 
   const projectSide = document.createElement('div');
-  projectSide.classList.add('side');
+  projectSide.classList.add('side', 'projects-side');
   projectSide.appendChild(projectForm());
   const listContainer = document.createElement('div');
   bar(listContainer, projectMemory);
   projectSide.appendChild(listContainer);
 
+  const taskSide = document.createElement('div');
+  taskSide.classList.add('side', 'tasks-side');
+  taskSide.appendChild(taskForm());
+  const taskContainer = document.createElement('div');
+  bar(taskContainer, projectMemory);
+  taskSide.appendChild(taskContainer);
+
   main.appendChild(projectSide);
+  main.appendChild(taskSide);
 
   return body;
 };
