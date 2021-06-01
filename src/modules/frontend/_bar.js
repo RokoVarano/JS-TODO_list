457ab242deef1893f674a80a74008d2f3eabea02
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-06-01 19:00:55 -0400

    Add Date to Task Widget

--- src/modules/frontend/_bar.js
+++ src/modules/frontend/_bar.js
@@ -77,9 +77,17 @@ const taskWidget = (parent, item, memProject) => {
   const container = document.createElement('div');
   container.classList.add('list-item');
 
+  const textContainer = document.createElement('div');
+
   const h3 = document.createElement('h3');
-  h3.textContent = item.title;
-  container.appendChild(h3);
+  h3.textContent = `${item.title}`;
+  textContainer.appendChild(h3);
+
+  const date = document.createElement('h3');
+  date.textContent = `${item.date}`;
+  textContainer.appendChild(date);
+
+  container.appendChild(textContainer);
 
   const xButton = document.createElement('button');
   xButton.classList.add('x-button');
