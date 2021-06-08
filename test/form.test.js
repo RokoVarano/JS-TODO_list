093943dec5c0118d12dd68459cf7d06811cfeb3a
oxenprogrammer <emmy1000okello@gmail.com>
2021-06-08 22:03:14 +0300

    Add test query form element

--- test/form.test.js
+++ test/form.test.js
@@ -5,15 +5,21 @@ import { projectForm, taskForm } from '../src/modules/frontend/_form';
 
 describe('Form', () => {
   const task = taskForm();
+  document.body.appendChild(task);
+  const form = task.querySelector('.form');
+  
   const project = projectForm();
+  document.body.appendChild(project);
 
   it('should create a form section', () => {
-    document.body.appendChild(task);
     expect(task.tagName).toBe('SECTION');
   });
 
   it('should create a form section', () => {
-    document.body.appendChild(project);
     expect(project.tagName).toBe('SECTION');
+  });
+
+  it('should return a form element', () => {
+    expect(form.tagName).toBe('FORM');
   });
 });
